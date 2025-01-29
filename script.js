0a7eaed9a8718d55af8e6de302c1109dcbb03a3e
shubhamdhatwalia1@gmail.com <shubhamdhatwalia1@gmail.com>
2025-01-29 19:42:28 +0530

    practice

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
 import { productData } from "./productData.js";
 
 
-
-
-
-
-
 var dropdown = document.getElementsByClassName("dropdown-btn");
+
 
 var i;
 
@@ -15,7 +11,7 @@ for (i = 0; i < dropdown.length; i++) {
 
     var dropdownContent = this.nextElementSibling;
     var dropdownIcon = this.children[0];
-    
+
 
     if (dropdownContent.style.display === "block") {
       dropdownContent.style.display = "none";
@@ -38,7 +34,7 @@ for (i = 0; i < dropdown.length; i++) {
 productData.forEach((product) => {
   const cardHtml = `
 
-  <swiper-slide class="productSlides">
+  <div class="productSlides swiper-slide">
                         <div class="productItem">
 
   <div class="item-card">
@@ -70,7 +66,7 @@ productData.forEach((product) => {
                         </div>
                         </div>
                         
-                    </swiper-slide>
+                    </div>
   `;
 
 
@@ -83,67 +79,91 @@ productData.forEach((product) => {
 
 
 
+const swiperConfig = {
+  slidesPerView: 4,
+  spaceBetween: 30,
+  breakpoints: {
+    1200: { slidesPerView: 4, spaceBetween: 30, slidesPerGroup: 4 },
+    992: { slidesPerView: 3, spaceBetween: 25, slidesPerGroup: 3 },
+    620: { slidesPerView: 2, spaceBetween: 10, slidesPerGroup: 2 },
+    200: { slidesPerView: 1, spaceBetween: 10, slidesPerGroup: 1 },
+  },
+};
 
+var falshSalesSwiper = new Swiper(".flash-sales-carousel ", {
+  ...swiperConfig,
+  navigation: {
+    nextEl: ".custom-next",  // Custom next button
+    prevEl: ".custom-prev",  // Custom previous button
+  },
 
-document.addEventListener('DOMContentLoaded', () => {
-  const swiperElement = document.querySelector('.salesSwiper');
-  const swiperElement2 = document.querySelector('.categorySwiper');
-  const swiperElement3 = document.querySelector('.productSwiper');
-
-
-  const swiper3 = swiperElement3.swiper;
-  const swiper2 = swiperElement2.swiper;
-  const swiper = swiperElement.swiper;
-
-
-
-document.querySelector('.products-prev').addEventListener('click', () => {
-
-    swiper3.slidePrev();
-});
- 
-document.querySelector('.products-next').addEventListener('click', () => {
-
-    swiper3.slideNext();
 });
 
 
+var bestSellingSwiper = new Swiper(".best-selling-carousel", {
+  ...swiperConfig,
+})
 
-  document.querySelector('.category-prev').addEventListener('click', () => {
-
-    swiper2.slidePrev();
-  });
-
-  document.querySelector('.category-next').addEventListener('click', () => {
-
-    swiper2.slideNext();
-  });
+var productsCarousel = new Swiper(".products-carousel", {
 
 
+  navigation: {
+    nextEl: ".products-next",
+    prevEl: ".products-prev"
+  },
+  grid: {
+    rows: 2,
+  },
+  breakpoints: {
+    1200: {
+      slidesPerView: 4, spaceBetween: 30, slidesPerGroup: 4, grid: { rows: 2 }},
+    992: { slidesPerView: 3, spaceBetween: 25, slidesPerGroup: 3, grid: { rows: 2 } },
+    620: { slidesPerView: 2, spaceBetween: 10, slidesPerGroup: 2, grid: { rows: 2 } },
+    200: { slidesPerView: 1, spaceBetween: 10, slidesPerGroup: 1, grid: { rows: 2 } },
+  },
+})
+
+var categoryCarousel = new Swiper(".categories-carousel", {
+  spaceBetween: 10,
+  slidesPerView: 6,
+  breakpoints: {
+    1440: { slidesPerView: 6, spaceBetween: 30, slidesPerGroup: 4 },
+    1024: { slidesPerView: 5, spaceBetween: 30, slidesPerGroup: 5 },
+    820: { slidesPerView: 4, spaceBetween: 25, slidesPerGroup: 4 },
+    620: { slidesPerView: 3, spaceBetween: 10, slidesPerGroup: 3 },
+    425: { slidesPerView: 2, spaceBetween: 10, slidesPerGroup: 2 },
+    200: { slidesPerView: 1, spaceBetween: 10, slidesPerGroup: 1 },
+  },
+  navigation: {
+    nextEl: ".category-next",  // Custom next button
+    prevEl: ".category-prev",  // Custom previous button
+  },
+})
 
 
-  document.querySelector('.custom-prev').addEventListener('click', () => {
+var sideNavCarousel = new Swiper(".nav-carousel", {
+  slidesPerView: 'auto',
+  spaceBetween: 40,
+  freeMode: true,
+  speed: 8000,
+  allowTouchMove: true,
+  
+  autoplay: {
+    delay: 0,
+    pauseOnMouseEnter: true,
+  }
+})
 
-    swiper.slidePrev();
-  });
-
-  document.querySelector('.custom-next').addEventListener('click', () => {
-
-    swiper.slideNext();
-  });
-});
 
 
 
 
-
-document.querySelector(".menu-btn").addEventListener("click", ()=>{
+document.querySelector(".menu-btn").addEventListener("click", () => {
   document.querySelector(".overlay").style.height = "100%";
 })
 
 
-document.querySelector(".closebtn").addEventListener("click", ()=>
-{
+document.querySelector(".closebtn").addEventListener("click", () => {
   document.querySelector(".overlay").style.height = "0";
 })
 
